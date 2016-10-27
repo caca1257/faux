01d5996f1d11b2e4754a8742ff3748796a7be092
Carolyn Castanon <carolyncastanon@Carolyns-iMac.local>
2016-10-26 21:14:49 -0600

    added navbar to be ro be responsive

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+// Populate dropdown with menu items
+/*$("nav a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo("nav select");
+});*/
+$("nav select").change(function() {
+  window.location = $(this).find("option:selected").val();
+});
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top //no need of parseInt here
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
